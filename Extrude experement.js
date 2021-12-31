08c93fb4984ed1b23db98c7b9ae3ab751a3f6862
Yuriy Gorvitovskiy <yuriy@gorvitovskiy.com>
2021-12-31 10:43:26 -0800

    Add rect-to-cycle extrusion example

--- Extrude experement.js
+++ Extrude experement.js
@@ -2,9 +2,9 @@ const jscad = require("@jscad/modeling");
 const { extrudeFromSlices, slice } = jscad.extrusions;
 const { geom2 } = jscad.geometries;
 const { mat4 } = jscad.maths;
-const { rectangle } = jscad.primitives;
+const { circle, rectangle } = jscad.primitives;
 
-const main = () => {
+const spiral = () => {
   const rect = slice.fromSides(geom2.toSides(rectangle({ size: [1, 2] })));
   const a = (Math.PI * 3) / 2;
   const d = 3;
@@ -24,4 +24,26 @@ const main = () => {
   );
 };
 
+const rect_cycle = () => {
+  const rect = slice.fromSides(geom2.toSides(rectangle({ size: [1, 2] })));
+  const circ = slice.transform(
+    mat4.fromZRotation(mat4.create(), (3 * Math.PI) / 4),
+    slice.fromSides(geom2.toSides(circle({ radius: 1, segments: 256 })))
+  );
+  //return cube({size: 1});
+  return extrudeFromSlices(
+    {
+      numberOfSlices: 2,
+      callback: (p, i, b) => {
+        const tr = mat4.fromTranslation(mat4.create(), [0, 0, i]);
+        return slice.transform(tr, b[i]);
+      },
+    },
+    [rect, circ]
+  );
+};
+
+const main = () => {
+  return rect_cycle();
+};
 module.exports = { main };
